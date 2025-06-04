63a47a82b50044691edfa27a5a79407925caf8ef
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-06-04 20:34:35 -0300

    Dash and animation

---
+++ Scenes/player.gd
@@ -0,0 +1,74 @@
+extends CharacterBody2D
+
+#MOVIMENTAÇÃO X ------
+const SPEED = 200.0
+var time_passed := 0.0  # Tempo acumulado para gerar o efeito senoidal
+#MOVIMENTAÇÃO Y -----
+const JUMP_VELOCITY = -300.0
+const JUMPS_MAX = 1;
+var jumps = JUMPS_MAX;
+var per_grav = true
+#Dash -----
+var dash = true
+var inDash = false
+const DASH_DURING = 0.3
+var cowdow_dash = 0.0
+
+func _physics_process(delta: float) -> void:
+	# Input horizontal
+	var direction_x := Input.get_axis("move_left", "move_right")
+	var direction_y := Input.get_axis("move_up", "move_down")
+	var space = Input.is_action_just_pressed("move_space")#se espaço foi apertado 1x
+	
+	var ground = is_on_floor()	#se estou no chão
+	
+	if space && dash:
+		inDash = true
+		dash = false
+		cowdow_dash = 0
+		velocity.y = direction_y * SPEED * 1.25
+		velocity.x = direction_x * SPEED * 1.25
+		per_grav = false
+		$Duck.scale = Vector2(1.1,0.9) #efeito visual
+	if inDash:
+		cowdow_dash += 1 * delta
+		if cowdow_dash >= DASH_DURING:
+			inDash = false
+			per_grav = true
+	else:
+		if direction_x:
+			# Movimento horizontal
+			velocity.x = direction_x * SPEED
+			$Duck.play("run")
+			# Efeito de balanço senoidal
+			time_passed += delta * 8.0  # Velocidade do balanço
+			var angle := sin(time_passed) * 8.0  # Oscila entre -10 e +10 graus
+			$Duck.rotation_degrees = angle
+
+			# Flip horizontal
+			$Duck.flip_h = direction_x < 0
+			$Duck.scale = Vector2.ONE  # Volta ao tamanho
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			$Duck.frame = 2
+			$Duck.stop()
+		# Reset ao parar
+			time_passed = 0.0
+			$Duck.rotation_degrees = 0.0
+			$Duck.scale = Vector2.ONE  # Volta ao tamanho
+	
+	if ground:
+		dash = true
+		jumps = JUMPS_MAX
+		
+	if per_grav:
+		if not ground:
+			if velocity.y > 0:
+				velocity += get_gravity() * delta * 1.75
+			else:
+				velocity += get_gravity() * delta
+		# Handle jump.
+	if Input.is_action_just_pressed("move_up") and jumps > 0:
+		velocity.y = JUMP_VELOCITY
+		jumps -= 1
+	move_and_slide()
