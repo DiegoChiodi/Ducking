a108061b876ee77552ac76959524c013f925dfbc
Diego Chiodi Gonçalves <dchiodigoncalves@gmail.com>
2025-06-06 16:56:29 -0300

    camera2D shake and player bouncing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -13,11 +13,17 @@ var dash = true
 var inDash = false
 const DASH_DURING = 0.3
 var cowdow_dash = 0.0
+#Direction -----------
+var direction = 1
+#Scale -----
+var targetScale = Vector2.ONE
 #Cenas
 var cenShadow = preload('res://Scenes/shadow.tscn')
 #Shadow
 const shadowsCreate = 3
-
+#Draw
+func _ready() -> void:
+	$aniDuck.z_index = 10
 func _physics_process(delta: float) -> void:
 	# Input horizontal
 	var direction_x := Input.get_axis("move_left", "move_right")
@@ -33,8 +39,9 @@ func _physics_process(delta: float) -> void:
 		velocity.y = direction_y * SPEED * 1.25
 		velocity.x = direction_x * SPEED * 1.25
 		per_grav = false
-		$aniDuck.scale = Vector2(1.1,0.9) #efeito visual
+		targetScale = Vector2(1.1,0.9) #efeito visual
 		$timShadow.start()
+		$Camera2D.start_shake(3, 12.0)  # intensidade, velocidade de queda
 	if inDash:
 		cowdow_dash += 1 * delta
 		
@@ -43,20 +50,20 @@ func _physics_process(delta: float) -> void:
 			per_grav = true
 			$timShadow.stop()
 			scenShadowCreate()
-		
 	else:
 		if direction_x:
 			# Movimento horizontal
-			velocity.x = direction_x * SPEED
+			velocity.x = direction * SPEED
 			$aniDuck.play("run")
 			# Efeito de balanço senoidal
 			time_passed += delta * 8.0  # Velocidade do balanço
 			var angle := sin(time_passed) * 8.0  # Oscila entre -10 e +10 graus
-			$aniDuck.rotation_degrees = angle
+			$aniDuck.rotation_degrees = angle   
 
+			direction = direction_x
+			$aniDuck.flip_h = (direction_x < 0)
 			# Flip horizontal
-			$aniDuck.flip_h = direction_x < 0
-			$aniDuck.scale = Vector2.ONE  # Volta ao tamanho
+			targetScale = Vector2.ONE  # Volta ao tamanho
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 			$aniDuck.frame = 2
@@ -64,7 +71,7 @@ func _physics_process(delta: float) -> void:
 		# Reset ao parar
 			time_passed = 0.0
 			$aniDuck.rotation_degrees = 0.0
-			$aniDuck.scale = Vector2.ONE  # Volta ao tamanho
+			targetScale = Vector2.ONE  # Volta ao tamanho
 	
 	if ground:
 		dash = true
@@ -74,14 +81,19 @@ func _physics_process(delta: float) -> void:
 		if not ground:
 			if velocity.y > 0:
 				velocity += get_gravity() * delta * 1.75
+				targetScale = Vector2(1.1, 0.9)  # caindo
 			else:
 				velocity += get_gravity() * delta
-		# Handle jump.
+				targetScale = Vector2(0.9, 1.1)  # subindo
+
+	# Handle jump.
 	if Input.is_action_just_pressed("move_up") and jumps > 0:
 		velocity.y = JUMP_VELOCITY
 		jumps -= 1
+	
+	# Interpola suavemente a escala para target_scale
+	$aniDuck.scale = $aniDuck.scale.lerp(targetScale, 8 * delta)
 	move_and_slide()
-
 func _on_tim_shadow_timeout() -> void:
 	scenShadowCreate()
 	
@@ -93,6 +105,6 @@ func scenShadowCreate():
 
 	insShadow.position = position + frame_size
 
-	insShadow.scale.x = scale.x * (1 - int($aniDuck.flip_h) * 2)
+	insShadow.scale.x = scale.x * direction
 	insShadow.scale.y = scale.y
 	get_tree().current_scene.add_child(insShadow)
